14da232f7f4aa06c0f9829f484a98477e457cd81
dekster113 <62512723+dekster113@users.noreply.github.com>
2020-05-12 21:48:55 +0200

    Konstruktor Book(Book book) w klasie Book, specyfikatory dostepu,gettery i settery

--- src/Book.java
+++ src/Book.java
@@ -1,21 +1,78 @@
+package app;
+
 public class Book {
-
-    String Tytul;
-    String Autor;
-    int DataWydania;
-    int Strony;
-    String Wydawca;
-    String isbn;
-
+// Pola
+    private String Tytul;
+    private String Autor;
+    private int DataWydania;
+    private int Strony;
+    private String Wydawca;
+    private String isbn;
+// Konstruktory
     public Book(String Tytul, String Autor, int DataWydania, int Strony, String Wydawca, String isbn) {
 
-    this.Tytul=Tytul;
-    this.Autor=Autor;
-    this.DataWydania=DataWydania;
-    this.Strony=Strony;
-    this.Wydawca=Wydawca;
-    this.isbn=isbn;
+        this.Tytul=Tytul;
+        this.Autor=Autor;
+        this.DataWydania=DataWydania;
+        this.Strony=Strony;
+        this.Wydawca=Wydawca;
+        this.isbn=isbn;
 }
+    Book (Book book)
+    {
+        this(book.Tytul, book.Autor, book.DataWydania, book.Strony, book.Wydawca, book.isbn);
+    }
+// set i get
+    public String getTytul() {
+        return Tytul;
+    }
+
+    public void setTytul(String Tytul) {
+        this.Tytul = Tytul;
+    }
+
+    public String getAutor(){
+      return Autor;
+    }
+
+    public void setAutor (String Autor){
+        this.Autor = Autor;
+    }
+
+    public int getDataWydania(){
+        return DataWydania;
+    }
+
+    public void setDataWydania (int DataWydania){
+        this.DataWydania = DataWydania;
+    }
+
+    public int getStrony(){
+        return Strony;
+    }
+
+    public void setStrony (int Strony){
+        this.Strony=Strony;
+    }
+
+    public String getWydawca(){
+        return Wydawca;
+    }
+
+    public void setWydawca(String Wydawca){
+        this.Wydawca=Wydawca;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn){
+        this.isbn=isbn;
+    }
+
+
+
     void DrukowanieInformacji ()
     {
         String info = Tytul+"; "+Autor+"; "+DataWydania+"; "+Strony+"; "+Wydawca+"; "+isbn;
--- src/Libary.java
+++ src/Libary.java
@@ -1,14 +1,17 @@
- class Library {
+package app;
+import app.Book;
+
+
+class Library {
     public static void main(String[] args) {
 
-        final String appName = "Biblioteka v0.3";
+        final String appName = "Biblioteka v0.4";
 
         Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, 296,
                 "Greg", "9788373271890");
         Book book2 = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch", 2009,
                 352, "Helion", "9788324620845");
-        Book book3 = new Book("Potop",
-                "Henryk Sienkiewicz", 2015, 936, "Greg",
+        Book book3 = new Book("Potop", "Henryk Sienkiewicz", 2015, 936, "Greg",
                 "9780071559825");
 
         System.out.print(appName);
