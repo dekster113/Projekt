6c8fbbaecb737f6f4d4947b759bcfac8153c1125
dekster113 <62512723+dekster113@users.noreply.github.com>
2020-06-11 13:23:50 +0200

    metody addBook(),addMagazine() zmieniono,metody printBooks() & printMagazines() wycofane,dodano Publication[]

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -3,7 +3,7 @@ import  app.LibraryControl;
 
 public class LibraryApp {
 
-    public static final String APP_NAME = "Biblioteka v1.0";
+    public static final String APP_NAME = "Biblioteka v1.1";
     public static void main(String[] args) {
         {
             System.out.println(APP_NAME);
